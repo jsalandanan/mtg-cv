e5ec47ba35ad31da0d0043a4a687ecf978f600c2
Jay Salandanan <jsalandanan@raybeam.com>
2019-07-08 22:53:06 -0700

    changes to try to understand at all

--- cv.py
+++ cv.py
@@ -20,19 +20,29 @@ sqKernel = cv2.getStructuringElement(cv2.MORPH_RECT, (21, 21))
 for imagePath in paths.list_images(args["images"]):
   # load the image, resize it, and convert it to grayscale
   image = cv2.imread(imagePath)
-  image = imutils.resize(image, height=600)
+  cv2.imshow("Image", image)
+  cv2.waitKey(0)
+  # image = imutils.resize(image, height=600) # how necessary and why?
   gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
- 
+  cv2.imshow("Gray", gray)
+  cv2.waitKey(0)
   # smooth the image using a 3x3 Gaussian, then apply the blackhat
   # morphological operator to find dark regions on a light background
-  gray = cv2.GaussianBlur(gray, (3, 3), 0)
+  gray = cv2.GaussianBlur(gray, (3, 3), 0) # LMAO is this dilation?
+  cv2.imshow("Gray", gray)
+  cv2.waitKey(0)
   blackhat = cv2.morphologyEx(gray, cv2.MORPH_BLACKHAT, rectKernel)
+  cv2.imshow("Blackhat", blackhat)
+  cv2.waitKey(0)
+
   # compute the Scharr gradient of the blackhat image and scale the
   # result into the range [0, 255]
   gradX = cv2.Sobel(blackhat, ddepth=cv2.CV_32F, dx=1, dy=0, ksize=-1)
   gradX = np.absolute(gradX)
   (minVal, maxVal) = (np.min(gradX), np.max(gradX))
   gradX = (255 * ((gradX - minVal) / (maxVal - minVal))).astype("uint8")
+  cv2.imshow("gradX", gradX)
+  cv2.waitKey(0)
   # apply a closing operation using the rectangular kernel to close
   # gaps in between letters -- then apply Otsu's thresholding method
   gradX = cv2.morphologyEx(gradX, cv2.MORPH_CLOSE, rectKernel)
